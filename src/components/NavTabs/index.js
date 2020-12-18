58a053b0c0809648ce88d6bff6ac2a28edeba5f3
idallas93 <Dallas.Isaac1@gmail.com>
2020-12-18 00:00:28 -0500

    updated navbar to include hamburger

--- src/components/NavTabs/index.js
+++ src/components/NavTabs/index.js
@@ -1,62 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, NavDropdown, Button } from "react-bootstrap";
-
+import { Nav, NavDropdown, Navbar, Button } from "react-bootstrap";
+import Logo from "../../pages/Assets/Logo.png";
 import "./style.css";
-
 
 const NavTabs = () => {
   return (
-    <Nav className="nav">
-      <Link to="/" id="isaac-dallas" className="nav-link">
-        Isaac Dallas
-      </Link>
-      <Link to="/about" className="nav-link">
-        About
-      </Link>
-      <Link to="/Portfolio" className="nav-link">
-        Portfolio
-      </Link>
-      <NavDropdown title="Social Media" id="basic-nav-dropdown">
-        <NavDropdown.Item href="https://www.linkedin.com/in/isaacjdallas/">
+    <Navbar collapseOnSelect expand="lg" variant="light" style={{background: "transparent"}}>
+      <Navbar.Brand href="#home">
         <img
-        src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-          LinkedIn
-        </NavDropdown.Item>
-        <NavDropdown.Item href="https://github.com/idallas93">
-        <img
-        src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-          Github
-        </NavDropdown.Item>
-        <NavDropdown.Item href=""></NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="https://stackexchange.com/users/19318034/isaac-dallas?tab=bookmarks">
-        <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/768px-Stack_Overflow_icon.svg.png"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-          Stack Overflow
-        </NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
+        src="https://pngarchive.com/public/uploads/preview/labrador-puppy-png-1001576265834jdhaarwx2n.png"
+          alt=""
+          width="50"
+          height="50"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="nav">
+          <Link to="/" id="isaac-dallas" className="nav-link">
+            Isaac Dallas
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/Portfolio" className="nav-link">
+            Portfolio
+          </Link>
+          <NavDropdown title="Social Media" id="basic-nav-dropdown">
+            <NavDropdown.Item href="https://www.linkedin.com/in/isaacjdallas/">
+              <img
+                src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+              LinkedIn
+            </NavDropdown.Item>
+            <NavDropdown.Item href="https://github.com/idallas93">
+              <img
+                src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+              Github
+            </NavDropdown.Item>
+            <NavDropdown.Item href=""></NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="https://stackexchange.com/users/19318034/isaac-dallas?tab=bookmarks">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Stack_Overflow_icon.svg/768px-Stack_Overflow_icon.svg.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+              Stack Overflow
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
 export default NavTabs;
-
-
-
-
