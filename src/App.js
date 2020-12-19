70c4c519fedcc1d9930de46703a7ccfd4436bd3d
idallas93 <Dallas.Isaac1@gmail.com>
2020-12-19 10:55:19 -0500

    added flex box and fixed background image styling

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import Portfolio from "./pages/Portfolio";
 import NavTabs from "./components/NavTabs";
 import FooterPage from "./components/Footer";
 import "./App.css";
+import coding from "./pages/Assets/coding.png"
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="wrapper">
+    <div className="App" style={{backgroundImage:`url(${coding})`}}>
       <div className="image-opacity">
         <Router>
-          <NavTabs />
-          <div id="app-container" className="container">
+          <NavTabs className="header"/>
+          <div id="article" className="container">
             <Route exact path="/" component={Home} />
             <Route exact path="/Home" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
           </div>
-          <FooterPage />
         </Router>
       </div>
+    </div>
+    <FooterPage className="footer"/>
     </div>
   );
 };
